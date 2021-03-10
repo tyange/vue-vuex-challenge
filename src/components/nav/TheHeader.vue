97485c8900efc3b1fa46af8f222721a460936d1f
babyazalea <usun12@hotmail.com>
2021-03-11 01:59:48 +0900

    using vuex

--- src/components/nav/TheHeader.vue
+++ src/components/nav/TheHeader.vue
@@ -10,7 +10,7 @@
         </li>
         <li>
           <router-link to="/cart">Cart</router-link>
-          <base-badge mode="elegant">{{ cart.qty }}</base-badge>
+          <base-badge mode="elegant">{{ cartQty }}</base-badge>
         </li>
         <li v-if="isLoggedIn">
           <router-link to="/admin">Admin</router-link>
@@ -26,7 +26,22 @@
 
 <script>
 export default {
-  inject: ['isLoggedIn', 'login', 'logout', 'cart'],
+  computed: {
+    isLoggedIn() {
+      return this.$store.getters.isAuthenticated;
+    },
+    cartQty() {
+      return this.$store.getters['cart/quantity'];
+    }
+  },
+  methods: {
+    login() {
+      this.$store.dispatch('login');
+    },
+    logout() {
+      this.$store.dispatch('logout');
+    }
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import router from './router.js';
 import App from './App.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
 
-const app = createApp(App)
+import store from './store/index';
+
+const app = createApp(App);
 
 app.use(router);
+
+app.use(store);
 
 app.component('base-badge', BaseBadge);
 
